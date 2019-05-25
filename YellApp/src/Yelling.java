4edc150aeca28b9cc6ac13bef8e7cd4d02bcd409
Mitali Ahir <mitaliahir3@gmail.com>
2019-05-24 23:49:45 -0400

    R2 Green

--- YellApp/src/Yelling.java
+++ YellApp/src/Yelling.java
@@ -5,6 +5,10 @@ public class Yelling {
 	//@param name array
 	public String scream(String[] name)
 	{
+		if(name == null)
+		{
+			return "Nobody is yelling";
+		}
 		String result = name[0] + " is yelling";
 		return result;
 	}
