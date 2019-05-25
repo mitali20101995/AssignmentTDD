00fd47aab0750b1abb77b30c2f7d8b1674629a7c
Mitali Ahir <mitaliahir3@gmail.com>
2019-05-25 00:31:48 -0400

    R4 Green No Refactor

--- YellApp/src/Yelling.java
+++ YellApp/src/Yelling.java
@@ -11,12 +11,21 @@ public class Yelling {
 		}
 		String s = names[0];
 		
+
 		if(s == s.toUpperCase())
 		{
 			return s + " IS YELLING";
 		}
+		
+		if(names.length == 2) {
+			String s2 = names[1];
+			return s + " and " + s2 + " are yelling";
+		}
 		String result = s + " is yelling";
 		return result;
+		
+		
+		
 	}
 
 }
